dfcd82b888708bc21b87f1381ac56a2a1b952d35
Donna Seidel <dseidel01@gmail.com>
2021-12-08 13:31:51 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_A_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to be reversed.");
+            string input = Console.ReadLine();
+            var resultstring = new string(input.ToCharArray().Reverse().ToArray());
+            Console.WriteLine(resultstring);
+        }
+    }
+}
